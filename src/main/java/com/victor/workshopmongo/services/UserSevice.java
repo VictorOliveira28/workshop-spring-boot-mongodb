e97bcfd31cb31bcea6e997fee0aabe5f6b31455b
Victor Oliveira <victor.oliveira20@icloud.com>
2023-08-14 18:58:20 -0300

    Inserção de usuário com POST

--- src/main/java/com/victor/workshopmongo/services/UserSevice.java
+++ src/main/java/com/victor/workshopmongo/services/UserSevice.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.victor.workshopmongo.domain.User;
+import com.victor.workshopmongo.dto.UserDTO;
 import com.victor.workshopmongo.repository.UserRepository;
 import com.victor.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,5 +26,13 @@ public class UserSevice {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);	
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+	}	
 
 }
