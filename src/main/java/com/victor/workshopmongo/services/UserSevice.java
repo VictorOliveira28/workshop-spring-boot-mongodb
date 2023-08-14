fcd7aad768d83a373400da1968601b62fb4b7dc2
Victor Oliveira <victor.oliveira20@icloud.com>
2023-08-14 18:46:25 -0300

    Obtendo um usuário por ID

--- src/main/java/com/victor/workshopmongo/services/UserSevice.java
+++ src/main/java/com/victor/workshopmongo/services/UserSevice.java
@@ -1,12 +1,14 @@
 package com.victor.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.victor.workshopmongo.domain.User;
 import com.victor.workshopmongo.repository.UserRepository;
+import com.victor.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserSevice {
@@ -16,6 +18,12 @@ public class UserSevice {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id){
+		
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
