60cd724980730106b0e7ce18c03c99a1d536b210
Victor Oliveira <victor.oliveira20@icloud.com>
2023-08-14 18:01:07 -0300

    Operação de instanciação da base de dados.

---
+++ src/main/java/com/victor/workshopmongo/config/Instatiation.java
@@ -0,0 +1,33 @@
+package com.victor.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.victor.workshopmongo.domain.User;
+import com.victor.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instatiation implements CommandLineRunner{
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+	}
+	
+	
+
+}
