d3f75acf0b8ad9fe7972ee9e378c3a8d4e99c376
Victor Oliveira <victor.oliveira20@icloud.com>
2023-08-14 19:22:26 -0300

    Atualização de usuário com PUT

--- src/main/java/com/victor/workshopmongo/services/UserSevice.java
+++ src/main/java/com/victor/workshopmongo/services/UserSevice.java
@@ -13,32 +13,46 @@ import com.victor.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserSevice {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
-	public User findById(String id){
-		
+
+	public User findById(String id) {
+
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public User insert(User obj) {
-		return repo.insert(obj);	
+		return repo.insert(obj);
 	}
-	
+
 	public void delete(String id) {
-		findById(id);		
+		findById(id);
 		repo.deleteById(id);
 	}
-	
+
+	public User update(User entity) {
+
+		User newObj = repo.findById(entity.getId()).get();
+
+		updateData(newObj, entity);
+
+		return repo.save(newObj);
+
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
-	}	
-
+	}
 
 }
