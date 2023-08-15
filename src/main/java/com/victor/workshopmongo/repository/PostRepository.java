c07082c446b84b8846136555250479e114627efa
Victor Oliveira <victor.oliveira20@icloud.com>
2023-08-15 13:36:21 -0300

    Consulta simples com @Query

--- src/main/java/com/victor/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/victor/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.victor.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.victor.workshopmongo.domain.Post;
@@ -10,5 +11,9 @@ import com.victor.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{	
 
+	@Query("{ 'title': { $regex: ?0/pattern/, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
 }
